e449306af69bafa649560be09bf4523be7743133
Naheun Alam <naheunpranto258@gmail.com>
2026-02-28 17:43:01 +0600

    section way hidden done

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ let currentTab = "all";
 const tabActive = ["bg-navy", "border-navy", "text-white"];
 const tabInactive = ["bg-transparent", "text-slate-700", "border-state-200", "text-black"];
 
-const allContainer = document.getElementById('allCards')
+const allContainer = document.getElementById('all-container')
 const interviewContainer = document.getElementById('interview-container')
 const rejectedContainer = document.getElementById('reject-container');
 
 
 function switchTab(tab){
+    
     const tabs = ["all", "interview", "rejected"];
 
     for(const t of tabs){
         const tabName = document.getElementById("tab-" + t);
         if(t === tab){
-            tabName.classList.remove(...tabInactive);
-            tabName.classList.remove(...tabActive);
+            tabName.classList.remove( ... tabInactive);
+            tabName.classList.add( ... tabActive);
         } else{
-            tabName.classList.remove(...tabActive);
-            tabName.classList.add(...tabInactive);
+            tabName.classList.remove( ... tabActive);
+            tabName.classList.add( ... tabInactive);
         }
     }
 
-    const pages = [allCards, interviewContainer, rejectContainer];
+    const pages = [allContainer, interviewContainer, rejectedContainer];
 
     for(const section of pages){
         section.classList.add("hidden");
@@ -35,7 +36,7 @@ function switchTab(tab){
         interviewContainer.classList.remove("hidden");
     }
     else{
-        rejectContainer.classList.remove("hidden");
+        rejectedContainer.classList.remove("hidden");
     }
 }
 
